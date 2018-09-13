b988f35943bb1acb9cb27ca4fd21d398ebd0131e
Timothy Platt <timothyplatt@timothys-mbp>
2018-09-13 18:09:03 +0100

    Initial Commit

---
+++ Paydown Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paydown Calculator
+//
+//  Created by Timothy Platt on 13/09/2018.
+//  Copyright © 2018 Timothy Platt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
