9f7771bc892f9eccc4619d8df07bc3577faf348d
Timothy Platt <timothyplatt@timothys-mbp>
2018-09-27 20:26:54 +0100

    Add increment/decrement buttons.

--- Paydown Calculator/Controller/TestViewController2ViewController.swift
+++ Paydown Calculator/Controller/TestViewController2ViewController.swift
@@ -13,8 +13,18 @@ class TestViewController2ViewController: UIViewController {
     
     @IBOutlet weak var progressRing: UICircularProgressRing!
     @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var differenceLabel: UILabel!
     @IBOutlet weak var stepperLabel: UILabel!
     @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var minusSymbolButton: UIButton!
+    @IBOutlet weak var plusSymbolButton: UIButton!
+    
+    @IBAction func minusSymbolButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func plusSymbolButtonAction(_ sender: Any) {
+    }
+    
     
     var balance: Double = 0
     var APR: Double = 0
@@ -28,31 +38,24 @@ class TestViewController2ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        summaryLabel.text = "Paying only the minimum will take you \(payDownTime) months to clear your balance and cost £\(cumulativeInterest)"
-        summaryLabel.numberOfLines = 0
-        summaryLabel.lineBreakMode = .byWordWrapping
-        
-        progressRing.maxValue = UICircularProgressRing.ProgressValue(Float(payDownTime))
-        progressRing.outerRingWidth = 12
-        progressRing.innerRingWidth = 12
-        progressRing.ringStyle = .dashed
-        progressRing.font = UIFont.boldSystemFont(ofSize: 40)
-        progressRing.valueIndicator = " months"
-        
         let firstMinPaymentRounded = firstMinPaymentAmount.rounded(.toNearestOrAwayFromZero)
         let firstMinPaymentAmountAsString = String(format: "%.0f", firstMinPaymentRounded)
         
         let balanceRounded = balance.rounded(.toNearestOrAwayFromZero)
         let balanceAsString = String(format: "%.0f", balanceRounded)
         
-        stepper.wraps = true
-        stepper.autorepeat = true
-        stepper.value = Double(firstMinPaymentAmountAsString)!-1
-        stepper.maximumValue = Double(balanceAsString)!
+        summaryLabel.text = "Paying only the minimum will take you \(payDownTime) months to clear your balance and cost £\(cumulativeInterest) in interest."
+        summaryLabel.numberOfLines = 0
+        summaryLabel.lineBreakMode = .byWordWrapping
+        
+        differenceLabel.numberOfLines = 0
+        differenceLabel.lineBreakMode = .byWordWrapping
+        
+        initialiseProgressRing()
+        initialiseStepper(startValue: Double(firstMinPaymentAmountAsString)!-1, maximumValue: Double(balanceAsString)!)
         
         test()
     }
-    
     
     @IBAction func stepperControl(_ sender: UIStepper) {
         
@@ -64,11 +67,12 @@ class TestViewController2ViewController: UIViewController {
         
         test1(monthsForFixed: Double(payDownTimeIfPayingFixedAmount.payDownTime))
         
+        //differenceLabel.text = "Paying a fixed amount of \(firstMinPaymentRounded) each month will clear your balance X months more quickly, saving you £Y"
+        
     }
 
     func test() -> Void {
-        // Somewhere not in viewDidLoad (since the views have not set yet, thus cannot be animated)
-        // Remember to use unowned or weak self if refrencing self to avoid retain cycle
+
         progressRing.startProgress(to: UICircularProgressRing.ProgressValue(Float(payDownTime)), duration: 2.0) {
             print("Done animating!")
             // Do anything your heart desires...
@@ -76,12 +80,34 @@ class TestViewController2ViewController: UIViewController {
     }
     
     func test1(monthsForFixed: Double) -> Void {
-        // Somewhere not in viewDidLoad (since the views have not set yet, thus cannot be animated)
-        // Remember to use unowned or weak self if refrencing self to avoid retain cycle
+
         progressRing.startProgress(to: UICircularProgressRing.ProgressValue(Float(monthsForFixed)), duration: 2.0) {
             print("Done animating!")
             // Do anything your heart desires...
         }
     }
 
+    
+    
+    
+    
+    
+    
+    
+    func initialiseProgressRing () -> Void {
+        progressRing.maxValue = UICircularProgressRing.ProgressValue(Float(payDownTime))
+        progressRing.outerRingWidth = 12
+        progressRing.innerRingWidth = 12
+        progressRing.ringStyle = .dashed
+        progressRing.font = UIFont.boldSystemFont(ofSize: 40)
+        progressRing.valueIndicator = " months"
+    }
+    
+    func initialiseStepper (startValue: Double, maximumValue: Double) -> Void {
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.value = startValue
+        stepper.maximumValue = maximumValue
+    }
+    
 }
